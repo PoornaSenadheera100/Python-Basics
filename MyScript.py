4ce673f80c265cd87c8533ed367a54dba87238d5
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-28 09:18:50 +0530

    Modified MyScript.py file

--- MyScript.py
+++ MyScript.py
@@ -1,6 +1,12 @@
+# Display "Hello World"
 print("Hello World")
+
+# Display "Hello World with an empty line"
 print("Hello World\n")
+
+# Keep an empty line
 print("\n")
 
+# Expression
 x = 1 + 2 ** 3 / 4 * 5
 print(x)
